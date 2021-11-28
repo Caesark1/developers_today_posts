f0c1c5e30a191d28e97ed83925e26de9fac1f526
Caesark1 <kebec2015@gmail.com>
2021-11-28 18:58:54 +0600

    tasks run everyday at 21-00

--- apps/posts/tasks.py
+++ apps/posts/tasks.py
@@ -4,7 +4,7 @@ from huey.contrib.djhuey import periodic_task
 from apps.posts.models import Post, Vote
 
 
-@periodic_task(crontab(minute='*/1'))
+@periodic_task(crontab(day='*/1'))
 def delete_posts_upvotes():
     Vote.objects.all().delete()
     Post.objects.all().update(votes_count=0)
