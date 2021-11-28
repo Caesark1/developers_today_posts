3da4b7048f425b67413a8d7e5d272c5be532466f
Caesark1 <kebec2015@gmail.com>
2021-11-28 20:25:31 +0600

    ready to production

--- developers_today/settings.py
+++ developers_today/settings.py
@@ -13,6 +13,7 @@ DEBUG = env.bool("DJANGO_DEBUG")
 
 ALLOWED_HOSTS = ["*"]
 
+ENVIRONMENT = env.str('ENVIRONMENT', default='development')
 
 # Application definition
 
@@ -137,3 +138,14 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+if ENVIRONMENT == 'production':
+    SECURE_BROWSER_XSS_FILTER = True
+    X_FRAME_OPTIONS = 'DENY'
+    SECURE_SSL_REDIRECT = True
+    SECURE_HSTS_SECONDS = 3600
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
